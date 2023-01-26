9252a43da05d7bbd59eeca44fc45f30a02c1275d
Joël Mik <contact@joelmik.nl>
2023-01-26 15:32:43 +0100

    marquee.

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,24 @@
+import Marquee from "react-fast-marquee";
 const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
         <div className="footer-top">
+          <Marquee
+            className="marquee"
+            gradient="false"
+            gradientColor=""
+            speed="120"
+          >
+            <span>
+              Web design — Interfaces — UI/UX — Branding — Content Management —
+              App design — Mobile design —
+            </span>
+            <span>
+              Web design — Interfaces — UI/UX — Branding — Content Management —
+              App design — Mobile design —
+            </span>
+          </Marquee>
           <h1>Interesse in een samenwerking?</h1>
           <div className="email-container" data-animation="to-top">
             <a href="mailto:joel@mikdevelopment.nl" className="send-email">
@@ -15,10 +31,7 @@ const Footer = () => {
           <div>
             <ul>
               <li>
-                <a
-                  href="https://instragram.com/mik_development"
-                  target="_blank"
-                >
+                <a href="https://instagram.com/mik_development" target="_blank">
                   Instagram
                 </a>
               </li>
