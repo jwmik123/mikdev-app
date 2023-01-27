ffd25c43e0eabf145df8b6219dff2e1fdaefbb55
Joël Mik <contact@joelmik.nl>
2023-01-27 21:20:02 +0100

    removed marquee to the outside of the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,53 +1,63 @@
 import Marquee from "react-fast-marquee";
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer-container">
-        <div className="footer-top">
-          <Marquee
-            className="marquee"
-            gradient="false"
-            gradientColor=""
-            speed="120"
-          >
-            <span>
-              Web design — Interfaces — UI/UX — Branding — Content Management —
-              App design — Mobile design —
-            </span>
-            <span>
-              Web design — Interfaces — UI/UX — Branding — Content Management —
-              App design — Mobile design —
-            </span>
-          </Marquee>
-          <h1>Interesse in een samenwerking?</h1>
-          <div className="email-container" data-animation="to-top">
-            <a href="mailto:joel@mikdevelopment.nl" className="send-email">
-              joel@mikdevelopment.nl
-            </a>
+    <>
+      <Marquee
+        className="marquee"
+        gradient="false"
+        gradientColor="orange"
+        speed="120"
+      >
+        <span>
+          Web design — Interfaces — UI/UX — Branding — Content Management — App
+          design — Mobile design —
+        </span>
+        <span>
+          Web design — Interfaces — UI/UX — Branding — Content Management — App
+          design — Mobile design —
+        </span>
+      </Marquee>
+      <footer>
+        <div className="footer-container">
+          <div className="footer-top">
+            <h1>Interesse in een samenwerking?</h1>
+            <div className="email-container" data-animation="to-top">
+              <span
+                className="send-email"
+                onClick={() => {
+                  navigator.clipboard.writeText("joel@mikdevelopment.nl");
+                }}
+              >
+                joel@mikdevelopment.nl
+              </span>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <span className="copy">&copy; Mik Development.</span>
+            <div>
+              <ul>
+                <li>
+                  <a
+                    href="https://instagram.com/mik_development"
+                    target="_blank"
+                  >
+                    Instagram
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/mik-development/"
+                    target="_blank"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="footer-bottom">
-          <span className="copy">&copy; Mik Development.</span>
-          <div>
-            <ul>
-              <li>
-                <a href="https://instagram.com/mik_development" target="_blank">
-                  Instagram
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/company/mik-development/"
-                  target="_blank"
-                >
-                  LinkedIn
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
