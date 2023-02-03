f5b80c392a12e97b74a022156a45a6600a1542d3
Joël Mik <contact@joelmik.nl>
2023-02-03 22:16:46 +0100

    changed protocol again... better fucking work

--- next.config.js
+++ next.config.js
@@ -2,11 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.mikdevelopment.nl"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "mik-development.s3.eu-central-1.amazonaws.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "www.mikdevelopment.nl",
         port: "",
       },
     ],
